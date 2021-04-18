cb5adf43549d69301d33839d765d0f9402099882
calvindito <calvindito7@gmail.com>
2021-04-18 15:47:55 +0700

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -13,38 +13,42 @@
 									</div>
 								</div>
 								<div class="col-md-3">
-									<h4 class="text-center">Contact</h4>
-									<div class="posts-sm row col-mb-30" id="post-list-footer">
-										<div class="entry col-12">
-											<div class="grid-inner">
-												<div class="entry-title">
-													<h4 class="text-center">
-														<a href="javascript:void(0);" class="font-weight-bold text-warning text-uppercase">Surabaya</a>
-													</h4>
-												</div>
-												<div class="entry-meta text-center">
-													<span class="text-center">Baliwerti 119-121 Kav. 10</span><br>
-													<span class="text-center">(+62) 811332642</span>
+									<div class="widget clearfix">
+										<h4 class="text-center">Contact</h4>
+										<div class="posts-sm row col-mb-30" id="post-list-footer">
+											<div class="entry col-12">
+												<div class="grid-inner">
+													<div class="entry-title">
+														<h4 class="text-center">
+															<a href="javascript:void(0);" class="font-weight-bold text-warning text-uppercase">Surabaya</a>
+														</h4>
+													</div>
+													<div class="entry-meta text-center">
+														<span class="text-center">Baliwerti 119-121 Kav. 10</span><br>
+														<span class="text-center">(+62) 811332642</span>
+													</div>
 												</div>
 											</div>
-										</div>
-										<div class="entry col-12">
-											<div class="grid-inner">
-												<div class="entry-title">
-													<h4 class="text-center">
-														<a href="javascript:void(0);" class="font-weight-bold text-warning text-uppercase">Jakarta</a>
-													</h4>
-												</div>
-												<div class="entry-meta text-center">
-													<span class="text-center">Tomang Raya No. 28-30</span><br>
-													<span class="text-center">(+62) 811257180</span>
+											<div class="entry col-12">
+												<div class="grid-inner">
+													<div class="entry-title">
+														<h4 class="text-center">
+															<a href="javascript:void(0);" class="font-weight-bold text-warning text-uppercase">Jakarta</a>
+														</h4>
+													</div>
+													<div class="entry-meta text-center">
+														<span class="text-center">Tomang Raya No. 28-30</span><br>
+														<span class="text-center">(+62) 811257180</span>
+													</div>
 												</div>
 											</div>
 										</div>
 									</div>
 								</div>
 								<div class="col-md-5">
-									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.9046259541456!2d112.73413071435085!3d-7.2516948732486535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7f943d5afbcf1%3A0x3677d8750a2f9a9b!2sKarya%20Modern!5e0!3m2!1sid!2sid!4v1618656372394!5m2!1sid!2sid" width="auto" height="280" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+									<div class="widget clearfix">
+										<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.9046259541456!2d112.73413071435085!3d-7.2516948732486535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7f943d5afbcf1%3A0x3677d8750a2f9a9b!2sKarya%20Modern!5e0!3m2!1sid!2sid!4v1618656372394!5m2!1sid!2sid" width="auto" height="280" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+									</div>
 								</div>
 							</div>
 						</div>
