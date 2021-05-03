87c82137e4aac47f42829fab5224521cdcbc5c37
Calvin Dito Pratama <calvindito7@gmail.com>
2021-05-03 13:28:08 +0700

    update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<body  onload="loadingOpen('body')">
+<body>
 	<div class="navbar navbar-expand-md navbar-dark">
 		<div class="navbar-brand">
 			<a href="{{ url('admin/dashboard') }}" class="d-inline-block">
