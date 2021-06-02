41fc47d37a6cbf823ae1ba8975a71e4320f23e42
Calvin Dito Pratmaa <calvindito7@gmail.com>
2021-06-03 00:33:53 +0700

    update

--- app/Models/CashBank.php
+++ app/Models/CashBank.php
@@ -21,37 +21,6 @@ class CashBank extends Model {
         'description'
     ];
 
-    public static function generateCode($type, $date)
-    {
-        if($type == 1) {
-            $total_str = 4;
-            $str_type  = 'CASH';
-        } else if($type == 2) {
-            $total_str = 4;
-            $str_type  = 'BANK';
-        } else if($type == 3) {
-            $total_str = 7;
-            $str_type  = 'JOURNAL';
-        }
-
-        $query = CashBank::selectRaw("RIGHT(code, 3) as code")
-            ->whereMonth('date', date('m', strtotime($date)))
-            ->whereYear('date', date('Y', strtotime($date)))
-            ->whereRaw("LEFT(code, $total_str) = ?", [$str_type])
-            ->orderByDesc('id')
-            ->limit(1)
-            ->get();
-
-        if($query->count() > 0) {
-            $code = (int)$query[0]->code + 1;
-        } else {
-            $code = '001';
-        }
-
-
-        return $str_type . '-' . date('my', strtotime($date)) . '-' . str_pad($code, 3, 0, STR_PAD_LEFT);
-    }
-
     public function user()
     {
         return $this->belongsTo('App\Models\User'); 
