b71972feef4ca7996434ff8fb76a1732eda9e526
Calvin Dito Pratama <calvindito7@gmail.com>
2021-07-01 15:18:05 +0700

    update

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -14,7 +14,7 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        require public_path('website/customers.php');
+        require public_path('website/backup.php');
 
         foreach($customers as $c) {
             Customer::insert([
