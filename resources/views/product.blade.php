da7a32f539531e1d75bb49fd08cdb8c1c54ceacf
Calvin Dito Pratama <calvindito7@gmail.com>
2021-08-18 16:47:58 +0700

    update

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -13,7 +13,7 @@
                                  <option value="">24</option>
                                  <option value="48" {{ $filter['other']['show'] == 48 ? 'selected' : '' }}>48</option>
                                  <option value="60" {{ $filter['other']['show'] == 60 ? 'selected' : '' }}>60</option>
-                                 <option value="90" {{ $filter['other']['show'] == 90 ? 'selected' : '' }}>90</option>
+                                 <option value="90" {{ $filter['other']['show'] == 100 ? 'selected' : '' }}>100</option>
                               </select>
                            </div>
                         </div>
